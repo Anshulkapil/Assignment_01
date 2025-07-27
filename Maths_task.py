704b663cdfd62e3f92f559da175d3765d174469d
Anshulkapil <anshulkapil9368@gmail.com>
2025-07-27 20:58:52 +0530

    Add files via upload

---
+++ Maths_task.py
@@ -0,0 +1,15 @@
+num1 = float(input('Enter the first number:'))
+num2 = float(input('Enter the second number:'))
+
+# For addition
+sum = num1 + num2
+print(f'Addition: ',sum)
+# For Subtract
+sub = num1 - num2
+print(f'Subtract: ',sub)
+# For Multiply;
+Multi = num1 * num2
+print(f'Multiply: ',Multi)
+# For Divide
+div = num1 / num2
+print(f'Division: ',div)
---
+++ greeting.py
@@ -0,0 +1,3 @@
+name = input('Enter your  first name: ')
+sur_name = input('Enter your last name: ')
+print(f'Hello, {name+sur_name}! Welcome to my python program')
